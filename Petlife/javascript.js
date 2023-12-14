108970bb39555262a70e291ba096c78ede89bee3
BiancaPuglisi <148987803+BiancaPuglisi@users.noreply.github.com>
2023-12-13 21:09:43 -0300

    Add files via upload

---
+++ Petlife/javascript.js
@@ -0,0 +1,6 @@
+var elementsDuvida = document.querySelectorAll('.duvida')
+elementsDuvida.forEach(function (duvida) {
+    duvida.addEventListener('click', function() {
+        duvida.classList.toggle('ativa')
+    })
+})
